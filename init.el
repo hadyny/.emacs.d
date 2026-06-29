59ea969920110e7cbfdd04586afe926665e62e2b
Hadyn Youens <hadyn.youens@educationperfect.com>
2026-06-29 15:20:58 +1200

    lsp tweaks

--- init.el
+++ init.el
@@ -1,11 +1,34 @@
 ;;; init.el --- Literate config loader -*- lexical-binding: t; -*-
 ;;; Commentary:
-;; Tangles and loads config.org.  All configuration lives there.
+;; Bootstraps straight.el and pins Org before tangling config.org.
+;; Pinning Org here avoids a built-in/straight version mismatch that
+;; occurs when `org-babel-load-file' pulls in the built-in Org first.
 ;;; Code:
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file nil 'nomessage))
+
+(setq straight-check-for-modifications nil)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package '(project :type built-in))
+(straight-use-package 'use-package)
+(straight-use-package 'org)
 
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
